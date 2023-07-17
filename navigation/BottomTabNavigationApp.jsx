d6932b27dbf6a5b61cd19bf6d64348e287ee078b
c0c0pang <coc1789@naver.com>
2023-07-18 01:56:44 +0900

    feat: 파일 위치 변경및 바텀 네비게이션 구성

--- navigation/BottomTabNavigationApp.jsx
+++ navigation/BottomTabNavigationApp.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Text, View } from 'react-native';
+import { FlatList, Text, View } from 'react-native';
 import HomeIcon from '../assets/HomeIcon.svg';
 import MessageIcon from '../assets/MessageIcon.svg';
 import ProfileIcon from '../assets/ProfileIcon.svg';
@@ -11,11 +11,24 @@ import ProfileCIcon from '../assets/ProfileCIcon.svg';
 import SearchIcon from '../assets/SearchIcon.svg';
 import MenuIcon from '../assets/MenuIcon.svg';
 import { WithLocalSvg } from 'react-native-svg';//svg사용에 필수
-import { HomeIconView } from './styled';
+import { GlobaScrolllView, HeaderLeftText, HomeIconView } from '../style/navigationStyle';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { DATA } from '../home/data';
+import { Item } from '../home/ProductCard';
+import { CardFlatList } from '../style/cardStyle';
 const Tab = createBottomTabNavigator();
 
 function HomeScreen() {
-    return <Text>HomeScreen</Text>;
+    return (
+        <GlobaScrolllView>
+            <CardFlatList
+                data={DATA}
+                renderItem={({ item }) => <Item title={item.title} image={item.img} price={item.price} />}
+                keyExtractor={item => item.id}
+            />
+        </GlobaScrolllView>
+    );
+
 }
 
 function MessageScreen() {
@@ -23,7 +36,12 @@ function MessageScreen() {
 }
 
 function ProfileScreen() {
-    return <Text>ProfileScreeㄴn</Text>;
+    return (
+        <SafeAreaView>
+            <Text>ProfileScreen</Text>
+        </SafeAreaView>
+
+    );
 }
 function BottomTabNavigationApp() {
     return (
@@ -31,32 +49,47 @@ function BottomTabNavigationApp() {
             <Tab.Navigator
                 initialRouteName="Home"
                 screenOptions={({ route }) => ({
+                    tabBarStyle: { backgroundColor: '#181a20', borderTopWidth: 0 },
                     tabBarIcon: ({ focused }) => {
-                        if (route.name === 'Home') {
+                        if (route.name === "Home") {
                             return <WithLocalSvg asset={focused ? (HomeCIcon) : (HomeIcon)} />;
                         }
 
-                        if (route.name === 'Message') {
+                        if (route.name === "Message") {
                             return <WithLocalSvg asset={focused ? (MessageCIcon) : (MessageIcon)} />;
                         }
-                        if (route.name === 'Profile') {
+                        if (route.name === "Profile") {
                             return <WithLocalSvg asset={focused ? (ProfileCIcon) : (ProfileIcon)} />;
                         }
                     },
                     headerRight: ({ onPress }) => {
-                        if (route.name == 'Home') {
+                        if (route.name === "Home") {
                             return (
-                                <View style={HomeIconView}>
+                                <HomeIconView>
                                     <WithLocalSvg asset={SearchIcon} />
                                     <WithLocalSvg asset={MenuIcon} />
-                                </View>
+                                </HomeIconView>
+                            )
+                        }
+                    },
+                    headerStyle: {
+                        backgroundColor: '#181a20',
+                        borderBottomWidth: 0,
+                    },
+                    headerLeft: () => {
+                        if (route.name === "Message") {
+                            return (
+                                <HeaderLeftText>채팅방</HeaderLeftText>
                             )
                         }
                     },
                     headerTitle: () => {
                         return <Text></Text>
                     },
-                    tabBarShowLabel: false
+                    tabBarShowLabel: false,
+                    headerShown: route.name === "Profile" ? (false) : (true),
+                    headerShadowVisible: false, // 헤더 border 제거
+
                 })}
             >
                 <Tab.Screen name="Home" component={HomeScreen} />
