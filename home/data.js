b795d5d8a28b6a9d0a42b03d4044ceaa85784001
c0c0pang <coc1789@naver.com>
2023-07-18 01:57:39 +0900

    feat: 상품에 대한 가상 데이터 제작

---
+++ home/data.js
@@ -0,0 +1,21 @@
+import DefaultImage from '../assets/DefaultImg.jpg'
+export const DATA = [
+    {
+        id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+        title: 'First Item',
+        price: 30000,
+        img: DefaultImage
+    },
+    {
+        id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+        title: 'Second Item',
+        price: 2000,
+        img: DefaultImage
+    },
+    {
+        id: '58694a0f-3da1-471f-bd96-145571e29d72',
+        title: 'Third Item',
+        price: 550000,
+        img: DefaultImage
+    },
+];
