67ef68d800805c4536588f780ec05c90811455aa
c0c0pang <coc1789@naver.com>
2023-07-24 14:31:08 +0900

    feat: 상품 카드 제작

--- home/ProductCard.jsx
+++ home/ProductCard.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import { CardImage, CardPriceText, CardTextView, CardTimeText, CardTitleText, CardView } from "../style/cardStyle";
-export const Item = (props) => (
-    <CardView>
-        <CardImage source={props.image} />
-        <CardTextView>
-            <CardTitleText>{props.title}</CardTitleText>
-            <CardPriceText>{props.price} 원</CardPriceText>
-            <CardTimeText>몇분 전</CardTimeText>
-        </CardTextView>
-    </CardView>
-)
+import { useNavigation } from '@react-navigation/native';
+import { WithLocalSvg } from 'react-native-svg';
+export const ProductCard = ({ props }) => {
+    const navigation = useNavigation();
+    const handleProductPress = (props) => {
+        navigation.navigate('ProductDetail', { props: props });
+    };
+    return (
+        <CardView onPress={() => handleProductPress(props)}>
+            <WithLocalSvg asset={props.img[0]} width={120} height={120} />
+            <CardTextView>
+                <CardTitleText>{props.title}</CardTitleText>
+                <CardPriceText>{props.price} 원</CardPriceText>
+                <CardTimeText>몇분 전</CardTimeText>
+            </CardTextView>
+        </CardView>
+    )
+}
