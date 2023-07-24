40459bb6e4334354ca50d37984738697c4ac9ce6
c0c0pang <coc1789@naver.com>
2023-07-24 14:33:49 +0900

    perf: 홈 화면의 기능을 따로 관리
    홈 화면의 stack을 이용하기 위해 파일 분리함

--- navigation/BottomTabNavigationApp.jsx
+++ navigation/BottomTabNavigationApp.jsx
@@ -1,48 +1,18 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { FlatList, Text, View } from 'react-native';
 import HomeIcon from '../assets/HomeIcon.svg';
 import MessageIcon from '../assets/MessageIcon.svg';
 import ProfileIcon from '../assets/ProfileIcon.svg';
 import HomeCIcon from '../assets/HomeCIcon.svg';
 import MessageCIcon from '../assets/MessageCIcon.svg';
 import ProfileCIcon from '../assets/ProfileCIcon.svg';
-import SearchIcon from '../assets/SearchIcon.svg';
-import MenuIcon from '../assets/MenuIcon.svg';
 import { WithLocalSvg } from 'react-native-svg';//svg사용에 필수
-import { GlobaScrolllView, HeaderLeftText, HomeIconView } from '../style/navigationStyle';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { DATA } from '../home/data';
-import { Item } from '../home/ProductCard';
-import { CardFlatList } from '../style/cardStyle';
+import { HeaderLeftText } from '../style/navigationStyle';
+import { MessageScreen } from '../message/MessageScreen';
+import { ProfileScreen } from '../profile/ProfileScreen';
+import { HomeStackScreen } from '../home/HomeStackScreen';
 const Tab = createBottomTabNavigator();
-
-function HomeScreen() {
-    return (
-        <GlobaScrolllView>
-            <CardFlatList
-                data={DATA}
-                renderItem={({ item }) => <Item title={item.title} image={item.img} price={item.price} />}
-                keyExtractor={item => item.id}
-            />
-        </GlobaScrolllView>
-    );
-
-}
-
-function MessageScreen() {
-    return <Text>MessageScreen</Text>;
-}
-
-function ProfileScreen() {
-    return (
-        <SafeAreaView>
-            <Text>ProfileScreen</Text>
-        </SafeAreaView>
-
-    );
-}
 function BottomTabNavigationApp() {
     return (
         <NavigationContainer>
@@ -54,27 +24,12 @@ function BottomTabNavigationApp() {
                         if (route.name === "Home") {
                             return <WithLocalSvg asset={focused ? (HomeCIcon) : (HomeIcon)} />;
                         }
-
                         if (route.name === "Message") {
                             return <WithLocalSvg asset={focused ? (MessageCIcon) : (MessageIcon)} />;
                         }
                         if (route.name === "Profile") {
                             return <WithLocalSvg asset={focused ? (ProfileCIcon) : (ProfileIcon)} />;
                         }
-                    },
-                    headerRight: ({ onPress }) => {
-                        if (route.name === "Home") {
-                            return (
-                                <HomeIconView>
-                                    <WithLocalSvg asset={SearchIcon} />
-                                    <WithLocalSvg asset={MenuIcon} />
-                                </HomeIconView>
-                            )
-                        }
-                    },
-                    headerStyle: {
-                        backgroundColor: '#181a20',
-                        borderBottomWidth: 0,
                     },
                     headerLeft: () => {
                         if (route.name === "Message") {
@@ -83,16 +38,13 @@ function BottomTabNavigationApp() {
                             )
                         }
                     },
-                    headerTitle: () => {
-                        return <Text></Text>
-                    },
                     tabBarShowLabel: false,
-                    headerShown: route.name === "Profile" ? (false) : (true),
+                    headerShown: false,
                     headerShadowVisible: false, // 헤더 border 제거
 
                 })}
             >
-                <Tab.Screen name="Home" component={HomeScreen} />
+                <Tab.Screen name="Home" component={HomeStackScreen} />
                 <Tab.Screen name="Message" component={MessageScreen} />
                 <Tab.Screen name="Profile" component={ProfileScreen} />
             </Tab.Navigator>
